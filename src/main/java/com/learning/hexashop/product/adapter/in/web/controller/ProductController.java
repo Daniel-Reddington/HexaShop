96e4ad88c840fe33635be635de2082dc7f4c5ba8
Daniel-Reddington <mialinirinadaniela@gmail.com>
2026-05-09 22:49:12 +0300

    feat: add ProductController for product creation and retrieval with UUID support

---
+++ src/main/java/com/learning/hexashop/product/adapter/in/web/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.learning.hexashop.product.adapter.in.web.controller;
+
+import com.learning.hexashop.product.adapter.in.web.dto.CreateProductRequest;
+import com.learning.hexashop.product.adapter.in.web.dto.ProductResponse;
+import com.learning.hexashop.product.domain.command.CreateProductCommand;
+import com.learning.hexashop.product.domain.model.ProductId;
+import com.learning.hexashop.product.domain.port.in.CreateProductUseCase;
+import com.learning.hexashop.product.domain.port.in.GetProductByIdUseCase;
+import com.learning.hexashop.product.domain.port.in.GetProductsUseCase;
+import com.learning.hexashop.shared.api.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final CreateProductUseCase createProductUseCase;
+    private final GetProductByIdUseCase getProductByIdUseCase;
+    private final GetProductsUseCase getProductsUseCase;
+
+    @PostMapping
+    public ApiResponse<ProductResponse> create(@RequestBody CreateProductRequest request) {
+        CreateProductCommand command = new CreateProductCommand(request.name(), request.price());
+
+        var response = createProductUseCase.create(command);
+
+        return ApiResponse.success(ProductResponse.from(response));
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<ProductResponse> getById(@PathVariable String id) {
+        var response = getProductByIdUseCase.getById(ProductId.fromString(id)).orElseThrow(() -> new RuntimeException("Product not found"));
+
+        return ApiResponse.success(ProductResponse.from(response));
+    }
+
+    @GetMapping
+    public ApiResponse<Page<ProductResponse>> getAll(@RequestBody Pageable pageable) {
+        var response = getProductsUseCase.getProducts(pageable).map(ProductResponse::from);
+
+        return ApiResponse.success(response);
+    }
+}
+
+
--- src/main/java/com/learning/hexashop/product/adapter/out/persistence/repository/JpaProductRepository.java
+++ src/main/java/com/learning/hexashop/product/adapter/out/persistence/repository/JpaProductRepository.java
@@ -3,5 +3,7 @@ package com.learning.hexashop.product.adapter.out.persistence.repository;
 import com.learning.hexashop.product.adapter.out.persistence.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
+import java.util.UUID;
+
+public interface JpaProductRepository extends JpaRepository<ProductEntity, UUID> {
 }
