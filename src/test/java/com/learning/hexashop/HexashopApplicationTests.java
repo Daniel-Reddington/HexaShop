725956e084e8a1864a85d2142c79599c81ea4110
Daniel-Reddington <mialinirinadaniela@gmail.com>
2026-05-09 12:33:49 +0300

    Merge local project with remote repo

---
+++ src/test/java/com/learning/hexashop/HexashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.hexashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
